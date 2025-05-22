1cde38e55f44414e454ac6073bdd73aadbabea05
evlachs <evlach.egor@gmail.com>
2025-05-22 23:09:07 +0300

    classificator updated. new dumps added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from classificator import Classificator
 
-dr = Classificator('dumps/telegram_audio_common_flow.pcapng', 'configs/classificator.yml')
-print(dr.config)
-session = dr.get_protocol_sessions('UDP')
-dr.get_session_parameters(session)
+dr = Classificator('dumps/telegram_audio_common_flow.pcapng', 'configs/services.yml')
+all_sessions = dr.get_protocol_sessions('UDP')
+
+for session_name, session in all_sessions.items():
+    params = dr.get_session_params('udp', session)
+    classification = dr.check_session_affiliation(session_name, params)
+    if classification[0]:
+        print(classification[1])
