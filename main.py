a9bb65ed88fc3aeeed99ca57344e3ff8417b6f9a
evlachs <evlach.egor@gmail.com>
2025-05-25 10:12:22 +0300

    main script updated

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from classificator import Classificator
-from config_handler import ConfigHandler
-from dump_handler import DumpHandler
+from config_handler import Config
+from dump_handler import Dump
+from datetime import datetime
 
-dump = DumpHandler('dumps/telegram_audio_common_flow.pcapng')
-print(list(dump.get_all_sessions())[0].split()[0])
 
-# if __name__ == '__main__':
-#     config = ConfigHandler('configs/services.yml')
-#
+if __name__ == '__main__':
+    start = datetime.now()
+    config = Config('configs/services.yml')
+    dump = Dump('dumps/telegram_audio_common_flow.pcapng')
+    config_params = config.classification_params
+    sessions = dump.get_all_sessions()
+    for session_name, session_packets in sessions.items():
+        classificator = Classificator(session_name, session_packets)
+        session_params = classificator.get_session_params()
+        result = classificator.classify_session(config_params, session_params)
+        print(result)
+    end = datetime.now()
