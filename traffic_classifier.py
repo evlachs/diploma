692bfd9643b68db56a5fb0ec06977b482d106fc4
evlachs <evlach.egor@gmail.com>
2025-05-31 13:49:19 +0300

    logging configured

--- traffic_classifier.py
+++ traffic_classifier.py
@@ -1,14 +1,26 @@
-from classificator import Classificator
-from config_handler import Config
-from dump_handler import Dump
+import logging
 import argparse
 from datetime import datetime
+from dump_handler import Dump
+from config_handler import Config
+from classificator import Classificator
+
+
+FORMAT = '%(asctime)s : %(filename)s : %(levelname)s : %(message)s'
+LEVEL = logging.INFO
 
 DESCRIPTION = 'Software for classifying network traffic in a pcap file'
 CONFIG_PATH = 'configs/services.yml'
-SESSIONS_PATH = datetime.now().strftime('%d.%m.%y')
+SESSIONS_PATH = f'saved_sessions/{datetime.now().strftime('%d.%m.%y')}'
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='logs/traffic_classifier.log', format=FORMAT)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(LEVEL)
+    console = logging.StreamHandler()
+    console.setLevel(LEVEL)
+    logger.addHandler(console)
+
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('input_file', help='path/to/input_file.pcap with packets for classification')
     parser.add_argument(
@@ -19,13 +31,6 @@ if __name__ == '__main__':
         help='path/to/config_file.yml with classification parameters'
     )
     parser.add_argument(
-        '-o',
-        '--classified_only',
-        action='store_false',
-        default=True,
-        help='displays classification result for all sessions'
-    )
-    parser.add_argument(
         '-s',
         '--sessions_path',
         action='store',
@@ -33,25 +38,44 @@ if __name__ == '__main__':
         help='path/to/directory for save classified sessions .pcap file'
     )
     parser.add_argument(
-        '-d',
-        '--save_disabled',
+        '-o',
+        '--classified_only',
         action='store_true',
         default=False,
-        help='disable sessions saving'
+        help='displays classification result for classified sessions only'
+    )
+    parser.add_argument(
+        '-d',
+        '--disable_saving',
+        action='store_true',
+        default=False,
+        help='disable classified sessions saving'
+    )
+    parser.add_argument(
+        '-a',
+        '--analyze',
+        action='store_true',
+        default=False,
+        help='work in session analyzer mode. displaying sessions params only'
     )
     args = parser.parse_args()
+
     config = Config(args.config_file)
-    dump = Dump(args.input_file)
     config_params = config.classification_params
+    dump = Dump(args.input_file)
     sessions = dump.get_all_sessions()
     for session_name, session_packets in sessions.items():
         classificator = Classificator(session_name, session_packets)
         session_params = classificator.get_session_params()
+        if args.analyze:
+            logger.info(f'{session_name}: {session_params}')
+            continue
         result = classificator.classify_session(config_params, session_params)
         if args.classified_only:
-            if result[0]: print(result[1])
+            if result[0]:
+                logger.info(result[1])
         else:
-            print(result[1])
-        if result[0] and not args.save_disabled:
-            print(args.save_disabled)
-            dump.save_session(session_packets, result[2], args.sessions_path)
+            logger.info(result[1])
+        if result[0] and not args.disable_saving:
+            saved_session_path = dump.save_session(session_packets, result[2], args.sessions_path)
+            logger.info(f'Session {session_name} saved in {saved_session_path}')
